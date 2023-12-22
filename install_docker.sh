7854ebbf3121aacf1c346dbe985ccbb2c84dc8c2
abhizeetyadav <ajeetusamni@gmail.com>
2023-12-21 22:37:44 -0500

    Docker Installation Complete

---
+++ install_docker.sh
@@ -0,0 +1,29 @@
+# Update package list
+sudo apt-get update
+
+# Install necessary packages to allow apt to use a repository over HTTPS
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Add stable Docker repository
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update the package database
+sudo apt-get update
+
+# Install Docker
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Add the current user to the docker group
+sudo usermod -aG docker $USER
+
+# Display Docker versionn
+docker --version
+
+
